558bc69f9b537047b1f8a6431174d0b9d662a804
Victoria <vickgoularte@gmail.com>
2023-02-25 19:07:41 -0300

    🆕 new component

--- features/home/src/main/java/com/viclab/home/presentation/HomeActivity.kt
+++ features/home/src/main/java/com/viclab/home/presentation/HomeActivity.kt
@@ -3,19 +3,24 @@ package com.viclab.home.presentation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.viclab.financialplanning.ui.theme.FinancialPlanningTheme
+import com.viclab.ui.components.CardValue
+import com.viclab.ui.theme.FinancialPlanningTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class HomeActivity : ComponentActivity() {
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,7 +31,7 @@ class HomeActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    HomeScreen()
                 }
             }
         }
@@ -34,14 +39,21 @@ class HomeActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun HomeScreen(modifier: Modifier = Modifier) {
+    Column(modifier = modifier
+        .verticalScroll(rememberScrollState())
+    ) {
+        CardValue("R$ 1.000,00")
+    }
 }
 
+@ExperimentalMaterial3Api
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     FinancialPlanningTheme {
-        Greeting("Android")
+        Scaffold { padding ->
+            HomeScreen(Modifier.padding(padding))
+        }
     }
 }
---
+++ ui/src/main/java/com/viclab/ui/components/CardValue.kt
@@ -0,0 +1,80 @@
+package com.viclab.ui.components
+
+import android.content.res.Configuration
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.viclab.ui.theme.FinancialPlanningTheme
+
+
+@Composable
+fun CardValue(
+    text: String,
+    modifier: Modifier = Modifier,
+) {
+    val customCardColors = CardDefaults.cardColors(
+        contentColor = MaterialTheme.colorScheme.secondary,
+        containerColor = MaterialTheme.colorScheme.secondaryContainer,
+        disabledContentColor = MaterialTheme.colorScheme.surface,
+        disabledContainerColor = MaterialTheme.colorScheme.onSurface,
+    )
+    val customCardElevation = CardDefaults.cardElevation(
+        defaultElevation = 8.dp,
+        pressedElevation = 2.dp,
+        focusedElevation = 4.dp
+    )
+
+    Card(
+        shape = CardDefaults.shape,
+        modifier = modifier.padding(16.dp),
+        colors = customCardColors,
+        elevation = customCardElevation
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(120.dp)
+                .padding(16.dp),
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            Column(
+                modifier = Modifier
+                .fillMaxWidth(),
+            ) {
+                Text(text = "Saldo",
+                    style = MaterialTheme.typography.labelLarge,
+                    modifier = Modifier
+                )
+                Spacer(modifier = Modifier.height(4.dp))
+                Text(text = text,
+                    style = MaterialTheme.typography.headlineMedium,
+                    modifier = Modifier
+                )
+            }
+        }
+    }
+}
+
+@Preview(name = "CardValue")
+@Preview(name = "CardValueDarkMode", uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Composable
+private fun PreviewCardValue() {
+    FinancialPlanningTheme {
+        Column(modifier = Modifier.fillMaxWidth()) {
+            CardValue("R$ 1.000,00")
+        }
+    }
+}
